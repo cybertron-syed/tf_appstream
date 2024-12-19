bd03fe80e3a9da017245a62cf6acff9dd70f6d02
Syed Shahinsha <syed@Syeds-MacBook-Pro.local>
2024-12-19 19:56:12 +0530

    appstream

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,13 @@
+# output "aws_vpc_endpoint" {
+#   value = {
+#     streaming = aws_vpc_endpoint.streaming.id
+#   }
+# }
+
+# output "aws_vpc_endpoint_streaming" {
+#   value = aws_vpc_endpoint.streaming.id
+# }
+
+output "vpce_id" {
+  value = aws_vpc_endpoint.streaming.id
+}
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+# output "vpce_id" {
+#   value = aws_vpc_endpoint.streaming.id
+# }
+
+# output "streaming_vpce_id" {
+#   value       = module.appstream.vpce_id
+#   description = "VPC Endpoint ID for AppStream streaming from child module"
+# }
